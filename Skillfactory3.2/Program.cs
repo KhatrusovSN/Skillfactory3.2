050aaae47c196ac492a5a29f85ccc9b6ae20d086
KhatrusovSN <113129616+KhatrusovSN@users.noreply.github.com>
2022-09-15 19:22:15 +0300

    Задание 3.3.2

--- Skillfactory3.2/Program.cs
+++ Skillfactory3.2/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 
-
 class MainClass
 {
 	public static void Main(string[] args)
 	{
 
-		string myName;
-		myName = "Sergey";
+		string MyName = "Sergey";
+		byte MyAge = 27;
+		bool HaveIApet = true;
+		double MyShoeSize = 42.5;
 
-		Console.WriteLine(myName);
-        Console.WriteLine('\x23');
+		Console.WriteLine("My name is " + MyName);
+		Console.WriteLine("MyAge " + MyAge);
+		Console.WriteLine("Do I have a pet? " + HaveIApet);
+		Console.WriteLine("My shoe size is " + MyShoeSize);
 
-		Console.ReadKey();
 	}
 }
