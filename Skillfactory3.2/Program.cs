e14e5e9c6a974fd60686194952764756185a937a
KhatrusovSN <113129616+KhatrusovSN@users.noreply.github.com>
2022-09-15 18:55:20 +0300

    Добавьте файлы проекта.

---
+++ Skillfactory3.2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+class MainClass
+{
+	public static void Main(string[] args)
+	{
+
+		string myName;
+		myName = "Sergey";
+
+		Console.WriteLine(myName);
+
+		Console.ReadKey();
+	}
+}
