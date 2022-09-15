0089c99ddcfbfc00c1705e3606491d894308a643
KhatrusovSN <113129616+KhatrusovSN@users.noreply.github.com>
2022-09-15 20:04:30 +0300

    Задание 3.7.4

--- Skillfactory3.2/Program.cs
+++ Skillfactory3.2/Program.cs
@@ -4,9 +4,13 @@ class MainClass
 {
 	public static void Main(string[] args)
 	{
-		double result = 10 % 3;
-        Console.WriteLine(result);
-
-
+		Console.Write("Enter your name: ");
+		string name = Console.ReadLine();
+		Console.Write("Enter your age: ");
+		byte age = checked((byte)int.Parse(Console.ReadLine()));
+		Console.WriteLine($"Your name is {name} and age is {age} ");
+        Console.WriteLine("What is your favorite day a week?");
+		DayOfWeek day = (DayOfWeek)int.Parse(Console.ReadLine());
+        Console.WriteLine("Your favorite day of week: " + day);
 	}
 }
